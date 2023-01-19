9602572ef5d9c4d780fd60ba8902568f93570284
Thiago Silva <thiagowfer@gmail.com>
2023-01-18 23:29:50 -0300

    change paragraphs and lists to links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,34 +8,34 @@ export function Footer() {
 
             <div className="col">
                 <h3>Explore Our Menu</h3>
-                <p>Menu</p>
-                <p>Nutrition</p>
-                <p>Ingredients</p>
-                <p>Drink Menu</p>
+                <p><a href="#menu">Menu</a></p>
+                <p><a href="#nutrition">Nutrition</a></p>
+                <p><a href="#ingredients">Ingredients</a></p>
+                <p><a href="#drink">Drink Menu</a></p>
             </div>
 
             <div className="col">
                 <h3>Get to know us</h3>
-                <p>About us</p>
-                <p>Franchising</p>
-                <p>Foundation</p>
-                <p>Mission Statement</p>
+                <p><a href="#menu">About us</a></p>
+                <p><a href="#menu">Franchising</a></p>
+                <p><a href="#menu">Foundation</a></p>
+                <p><a href="#menu">Mission Statement</a></p>
             </div>
 
             <div className="col">
                 <h3>Get in touch</h3>
-                <p>Press</p>
-                <p>Carrers</p>
-                <p>Contact us</p>
-                <p>Get Social</p>
+                <p><a href="#menu">Press</a></p>
+                <p><a href="#menu">Carrers</a></p>
+                <p><a href="#menu">Contact us</a></p>
+                <p><a href="#menu">Get Social</a></p>
             </div>
 
             <div>
                 <h3>Let's Connect</h3>
                 <div className="social">
-                    <FaFacebookSquare className="icon" />
-                    <FaInstagramSquare className="icon" />
-                    <FaTwitterSquare className="icon" />
+                    <a href="https://facebook.com"><FaFacebookSquare className="icon" /></a>
+                    <a href="https://facebook.com"><FaInstagramSquare className="icon" /></a>
+                    <a href="https://facebook.com"><FaTwitterSquare className="icon" /></a>
                 </div>
             </div>
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -16,17 +16,17 @@ export function Navbar() {
   }
 
   window.addEventListener('scroll', changeColor)
-  
+    
   return (
-    <div className={color ? 'navbar navbar-bg' : 'navbar'}>
+    <div className={color ? 'navbar navbar-bg' : 'navbar'} >
         <div className="container">
             <div><FaHamburger size={40} /></div>
             <ul className={nav ? 'nav-menu active' : 'nav-menu'}>
-                <li>Home</li>
-                <li>Order</li>
-                <li>Menu</li>
-                <li>Delivery</li>
-                <li>Contact</li>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#home">Order</a></li>  
+                <li><a href="#home">Menu</a></li>
+                <li><a href="#home">Delivery</a></li>
+                <li><a href="#home">Contact</a></li>
             </ul>
             <div className="hamburger" onClick={handleNav}>
                 {nav ? <FaTimes size={30} color="#fff" /> : <FaBars size={30}/>}
